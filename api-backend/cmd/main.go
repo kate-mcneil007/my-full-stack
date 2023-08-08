500f5afb5318201740ca2a4c406c17cea4da8095
Kathryn McNeil <kate.mcneil007@gmail.com>
2023-08-08 13:58:44 -0400

    Committing changes, just added gin-gonic

--- api-backend/cmd/main.go
+++ api-backend/cmd/main.go
@@ -1,21 +1,25 @@
 // Spins up HTTP Server
-// Go program package 
+// Go program package
 package main
 
-// Required packages for program 
+// Required packages for program
 import (
+	//"context"
 	"context"
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
-	"github.com/jackc/pgx/v5"
+
+	database "github.com/kate-mcneil007/my-full-stack/pkg/db"
+	//"github.com/jackc/pgx/v5"
 )
 
 // Both functions accept same args
-// This function signature is used for HTTP handler functions 
-// When a requuest is made to the server these two vals are set up with info about the request being made
+// This function signature is used for HTTP handler functions
+// When a request is made to the server these two vals are set up with info about the request being made
 // It then calls the handler func w/ those vals
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
@@ -27,18 +31,29 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// Calls setting u0p handler func for request path in default server multiplexer
+	ctx := context.Background()
+	conn, err := database.ConnectPostgreSqlDb(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close(ctx)
+	database.CreateItem(conn, &database.Item{
+		ProductName: "katie is cute",
+		Quantity:    666,
+		Price:       9.99,
+	})
+	// Calls setting up handler func for request path in default server multiplexer
 	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/hello", getHello)
 
-	// Once handlers set up this is called to tell global HTTP server to listen for incoming requests 
+	// Once handlers set up this is called to tell global HTTP server to listen for incoming requests
 	// Here we use port :3000
-	err := http.ListenAndServe(":3000", nil)
+	err = http.ListenAndServe(":3000", nil)
 	// Checking for shut down/ closed server
-	// Also used to show why server stopped 
+	// Also used to show why server stopped
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
-	// Checks any other error
+		// Checks any other error
 	} else if err != nil {
 		fmt.Printf("error starting server: %s\n", err)
 		os.Exit(1)
--- api-backend/pkg/db/db.go
+++ api-backend/pkg/db/db.go
@@ -1,18 +1,104 @@
+// Is this right?
+package database
+
 import (
-	"github.com/jackc/pgx/v5"
 	"context"
-	// Added this 
-	"fmt" 
+
+	"github.com/jackc/pgx/v4"
+
+	// Added this
+	"fmt"
 	"os"
 )
 
-func ConnectPostgreSqlDb(ctx context.Context) {
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+//	func ConnectPostgreSqlDb(ctx context.Context) {
+//		conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+//		if err != nil {
+//			fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+//			os.Exit(1)
+//		}
+//		defer conn.Close(ctx)
+//	}
+func ConnectPostgreSqlDb(ctx context.Context) (*pgx.Conn, error) {
+	url := os.Getenv("DATABASE_URL")
+	conn, err := pgx.Connect(ctx, url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("unable to connect to database: %w", err)
 	}
-	defer conn.Close()
+	return conn, nil
 }
-// Database operations go here...
+
 // Can use 'conn' object to execute inserts and queries.
+
+// Entry in inventory table
+type Item struct {
+	ID          int     `json:"id" db:"id"`
+	ProductName string  `json:"product_name" db:"product_name"`
+	Quantity    int     `json:"quantity" db:"quantity"`
+	Price       float64 `json:"price" db:"price"`
+}
+
+func CreateItem(conn *pgx.Conn, item *Item) error {
+	query := "INSERT INTO inventory (product_name, quantity, price) VALUES ($1, $2, $3)"
+
+	// Execute query using 'conn' object and the item's properties
+	_, err := conn.Exec(context.Background(), query, item.ProductName, item.Quantity, item.Price)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ReadItems(conn *pgx.Conn) ([]Item, error) {
+	var items []Item
+
+	query := "SELECT id, product_name, quantity, price FROM inventory"
+
+	// Execute query using 'conn' object
+	rows, err := conn.Query(context.Background(), query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Iterate through the result rows and populate the items slice
+	for rows.Next() {
+		var item Item
+		err := rows.Scan(&item.ID, &item.ProductName, &item.Quantity, &item.Price)
+		if err != nil {
+			return nil, err
+		}
+		items = append(items, item)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return items, nil
+}
+
+func UpdateItem(conn *pgx.Conn, item *Item) error {
+	query := "UPDATE inventory SET product_name = $1, quantity = $2, price = $3 WHERE id = $4"
+
+	// Execute query using 'conn' obj and the item's properties
+	_, err := conn.Exec(context.Background(), query, item.ProductName, item.Quantity, item.Price, item.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func DeleteItem(conn *pgx.Conn, itemID int) error {
+	query := "DELETE FROM inventory WHERE id = $1"
+
+	// Execute query using 'conn' obj & item ID
+	_, err := conn.Exec(context.Background(), query, itemID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
